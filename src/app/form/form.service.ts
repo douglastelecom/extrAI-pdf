6ff7f39a79509a213993f9df7f8f47942b978fcf
Douglas <douglastelec@gmail.com>
2024-04-20 12:52:35 -0300

    sassa

--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -9,12 +9,12 @@ export class FormService {
   constructor(private http: HttpClient) { }
 
   async testApi(form: any){
-      var response = this.http.post<any>('https://extrai-pdf-back.onrender.com:8080/test', form)
+      var response = this.http.post<any>('https://extrai-pdf-back.onrender.com/test', form)
       return await lastValueFrom(response)
   }
 
   async completion(formData: any){
-      var response = this.http.post<any>('https://extrai-pdf-back.onrender.com:8080/completion', formData)
+      var response = this.http.post<any>('https://extrai-pdf-back.onrender.com/completion', formData)
       return await lastValueFrom(response)
   }
 }
