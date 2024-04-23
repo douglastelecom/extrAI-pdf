e9baedb51d0e947b38110ebe2c4bea93d215a3e6
Douglas F <douglastelec@gmail.com>
2024-04-23 13:46:09 -0300

    asas

--- src/app/form/form.service.ts
+++ src/app/form/form.service.ts
@@ -9,17 +9,17 @@ export class FormService {
   constructor(private http: HttpClient) { }
 
   async testApi(form: any){
-      var response = this.http.post<any>('http://localhost:8080/test', form)
+      var response = this.http.post<any>('https://extrai-pdf-back-homologa.onrender.com/test', form)
       return await lastValueFrom(response)
   }
 
   async completion(formData: any){
-      var response = this.http.post<any>('http://localhost:8080/completion', formData)
+      var response = this.http.post<any>('https://extrai-pdf-back-homologa.onrender.com/completion', formData)
       return await lastValueFrom(response)
   }
 
   async healthCheck(){
-    var response = this.http.get<any>('http://localhost:8080/').subscribe()
+    var response = this.http.get<any>('https://extrai-pdf-back-homologa.onrender.com/').subscribe()
 }
 
 }
